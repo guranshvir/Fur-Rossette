12b96669671809a973d44e729299d940ef933f9b
guranshvir <guranshvir@gmail.com>
2024-07-10 01:03:39 -0700

    Check

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir: "build"
-  },
-  assetsInclude: ["**/*.glb","**/*.MP3" ]
+  build: {
+    rollupOptions: {
+        external: [
+            "react", // ignore react stuff
+            "react-dom",
+        ],
+    }
+},
+  assetsInclude: ["**/*.glb","**/*.MP3" ],
 })
