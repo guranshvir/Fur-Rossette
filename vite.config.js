b0a882a9838b0a0e3956f361f5c48da3477d5b1e
guranshvir <guranshvir@gmail.com>
2024-07-10 00:41:52 -0700

    The root changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb","**/*.MP3" ],
+  build:{
+    outDir: "build"
+  },
+  assetsInclude: ["**/*.glb","**/*.MP3" ]
 })
